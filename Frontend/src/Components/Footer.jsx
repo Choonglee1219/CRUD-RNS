29c7f43c2b550f725d67b153c077217e5f60d61f
Choonglee1219 <81548639+Choonglee1219@users.noreply.github.com>
2024-05-27 18:56:06 +0900

    v.1.1_concurrently added

--- Frontend/src/Components/Footer.jsx
+++ Frontend/src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import "../Styles/Footer.css"
 export default function Footer() {
   return (
     <footer>
-        <p className="copyright">2023 - LC Codes</p>
+        <p className="copyright">2024 - Kepco-ENC</p>
     </footer>
   )
 }
